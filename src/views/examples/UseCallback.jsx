6e3e7992f6566e4056c4893bb38a553e2bc8efa3
beatrizartimundo <beatriz.artimundo@gmail.com>
2021-11-24 07:55:11 -0300

    useCallback e React.memo

--- src/views/examples/UseCallback.jsx
+++ src/views/examples/UseCallback.jsx
@@ -1,13 +1,30 @@
-import React from 'react'
+import React, { useCallback, useState } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
+import UseCallbackButtons from './useCallbackButtons';
 
-const UseCallback = (props) => {
+
+const UseCallback = props => {
+    const [count, setCount] = useState(0)
+    
+    //sem useCallback, renderiza varias vezes pois considera como uma funcao 'diferente'
+    // const inc = (delta) =>{
+    //     setCount(curr => curr + delta)
+    // }
+
+    const inc = useCallback(function (delta) {
+        setCount(curr => curr + delta)
+    }, [setCount])
+
     return (
         <div className="UseCallback">
             <PageTitle
                 title="Hook UseCallback"
-                subtitle="Retorna uma função memoizada!"
-            />
+                subtitle="Retorna uma função memoizada!" />
+
+            <div className="center">
+                <span className="text">{count}</span>
+                <UseCallbackButtons inc={inc} />
+            </div>
         </div>
     )
 }
